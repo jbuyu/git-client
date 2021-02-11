b1ab03b4c367192187640f51db81035851b1701b
jbuyu <jbuyu@ymail.com>
2021-02-11 19:49:53 +0300

    adds individual buttons to table

--- src/components/TopRepos.js
+++ src/components/TopRepos.js
@@ -1,37 +1,30 @@
 import React, { Component } from "react";
 import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
+import { Button } from "react-bootstrap";
 
 class TopRepo extends Component {
-  //   renderRepo = (repo, index) => {
-  //     return (
-  //       <tr key={index}>
-  //         <td colspan="1">{repo.name}</td>
-  //         <td colspan="2">{repo.description}</td>
-  //         <td colspan="1">{repo.stargazers_count}</td>
-  //       </tr>
-  //     );
-  //   };
+  cellButton(cell, row, enumObject) {
+    return (
+      <Button
+        onClick={this.showDetails.bind(this, row)}
+        cell={cell}
+        row={row}
+        // rowIndex={rowIndex}
+      >
+        View
+      </Button>
+    );
+  }
+  showDetails = (row) => {
+    console.log(row);
+  };
   render() {
     let { topRepos } = this.props;
     console.log(topRepos);
     return (
       <>
         <h3>Top Repos</h3>
-        {/* <Table striped condensed hover>
-          <thead>
-            <tr className="align-items-center">
-              <th></th>
-              <th colspan="1">Name</th>
-              <th colspan="3">Description</th>
-              <th colspan="1">Stars</th>
-            </tr>
-          </thead>
-          {topRepos && (
-            <tbody className="align-items-center">
-              {topRepos.map(this.renderRepo)}
-            </tbody>
-          )}
-        </Table> */}
+
         <div className="ml-5 mr-5">
           <BootstrapTable data={topRepos}>
             <TableHeaderColumn width={"10%"} dataField="name" isKey>
@@ -46,6 +39,10 @@ class TopRepo extends Component {
             <TableHeaderColumn dataField="open_issues_count">
               Open Issues
             </TableHeaderColumn>
+            <TableHeaderColumn
+              dataField="sessionDetails"
+              dataFormat={this.cellButton.bind(this)}
+            ></TableHeaderColumn>
           </BootstrapTable>
         </div>
       </>
