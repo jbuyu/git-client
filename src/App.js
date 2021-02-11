1355541afe4d72b512f712135216086eaaf00866
jbuyu <jbuyu@ymail.com>
2021-02-11 18:22:10 +0300

    adds url for top repos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import React, { Component } from "react";
 import "./App.css";
 import TopRepo from "./components/TopRepos";
+import Axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <TopRepo />
-    </div>
-  );
+class App extends Component {
+  state = {
+    topRepos: {},
+  };
+  componentDidMount() {
+    Axios.get(
+      `https://api.github.com/search/repositories?q=stars:%3E1&sort=stars`
+    ).then((res) => {
+      const topRepos = res.data.items;
+      this.setState({ topRepos });
+      console.log(topRepos);
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <TopRepo />
+      </div>
+    );
+  }
 }
 
 export default App;
