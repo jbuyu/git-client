9f035156f77958519d4eb84eb4bac7909f19619c
jbuyu <jbuyu@ymail.com>
2021-02-11 23:34:18 +0300

    fixes on routes

---
+++ src/components/Favourites.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import Axios from "axios";
+import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
+
+class Favourites extends Component {
+  state = {
+    repos: [],
+  };
+  componentDidMount() {
+    Axios.get(`http://localhost:4000/favRepos`).then((res) => {
+      const { result } = res.data;
+      this.setState({ repos: result });
+      console.log(result);
+    });
+  }
+  render() {
+    let repos = this.state;
+    console.log("look", repos.repos);
+    return (
+      <div className="favs">
+        <h3>Favs</h3>
+        <div className="ml-5 mr-5">
+          <BootstrapTable data={repos.repos}>
+            <TableHeaderColumn width={"10%"} dataField="owner" isKey>
+              Name
+            </TableHeaderColumn>
+            <TableHeaderColumn width={"50%"} dataField="repoName">
+              Description
+            </TableHeaderColumn>
+            <TableHeaderColumn dataField="stars">Stars</TableHeaderColumn>
+            <TableHeaderColumn dataField="issues">
+              Open Issues
+            </TableHeaderColumn>
+            <TableHeaderColumn dataField="forks">Forks</TableHeaderColumn>
+          </BootstrapTable>
+        </div>
+      </div>
+    );
+  }
+}
+export default Favourites;
--- src/index.js
+++ src/index.js
@@ -5,8 +5,25 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Repo from "./components/Repo";
+import Favourites from "./components/Favourites";
+import { NavLink } from "react-router-dom";
+
+const activeStyle = { color: "#F15B2A" };
 ReactDOM.render(
   <Router>
+    <nav className="mt-5 d-flex justify-content-center">
+      <NavLink to="/" activeStyle={activeStyle} exact>
+        Home
+      </NavLink>
+      {" | "}
+      {/* <NavLink to="/repos" activeStyle={activeStyle}>
+        Repos
+      </NavLink> */}
+      {" | "}
+      <NavLink to="/favs" activeStyle={activeStyle}>
+        Favourites
+      </NavLink>
+    </nav>
     <Switch>
       <Route exact path="/">
         <App />
@@ -16,6 +33,9 @@ ReactDOM.render(
         exact
         render={(props) => <Repo {...props} />}
       />
+      <Route exact path="/favs">
+        <Favourites />
+      </Route>
     </Switch>
   </Router>,
   document.getElementById("root")
