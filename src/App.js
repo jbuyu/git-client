3189ed5cb12fecaaa48371e59bbe8e480ff90cbb
jbuyu <jbuyu@ymail.com>
2021-02-11 18:15:51 +0300

    adds axios, sets up repo component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import TopRepo from "./components/TopRepos";
 
 function App() {
   return (
     <div className="App">
-     <p>REPOS</p>
+      <TopRepo />
     </div>
   );
 }
---
+++ src/components/TopRepos.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class TopRepo extends Component {
+  render() {
+    return <h3>Top Repos</h3>;
+  }
+}
+export default TopRepo;
